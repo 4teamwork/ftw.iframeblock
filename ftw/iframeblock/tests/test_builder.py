affa13eecadabc7946aa8f65cb8b4a12a6ed10de
Matthias Osswald <m@osswald.li>
2019-03-22 09:16:07 +0100

    Fix naming and add temporary test

--- ftw/iframeblock/tests/test_builder.py
+++ ftw/iframeblock/tests/test_builder.py
@@ -2,6 +2,7 @@ from ftw.builder import Builder
 from ftw.builder import create
 from ftw.testbrowser import browsing
 from ftw.iframeblock.tests import FunctionalTestCase
+from ftw.testing import IS_PLONE_5
 
 
 class TestIFrameBlockBuilder(FunctionalTestCase):
@@ -12,11 +13,22 @@ class TestIFrameBlockBuilder(FunctionalTestCase):
 
     @browsing
     def test_add_iframeblock(self, browser):
+        block_title = u'My iFrame block'
         content_page = create(Builder('sl content page'))
         create(Builder('iframe block')
-               .titled(u'My iFrame block')
+               .titled(block_title)
                .within(content_page))
 
         browser.login().visit(content_page)
 
-        self.assertTrue(len(browser.css('.sl-block')), 'Expect one block')
+        if IS_PLONE_5:
+            nodes = content_page.getChildNodes()
+            iframe_block_title = nodes._data[0].title
+
+            self.assertEqual(block_title, iframe_block_title,
+                             'We expect that a iframe block was created '
+                             'within the content page. Therefore the block '
+                             'must be the only child element of the content '
+                             'page having the title given.')
+        else:
+            self.assertTrue(len(browser.css('.sl-block')), 'Expect one block')
