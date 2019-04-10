f93bc19b91226ca6c91dc4d11dc6491774b14449
Matthias Osswald <m@osswald.li>
2019-04-10 14:46:32 +0200

    Undo plone5 specific testing because wrong
    Probably the properties not registered leaded to the issue why I
    initially changed the test. This has been fixed. Therefore we
    now can test for the css selector of the iframe again.

--- ftw/iframeblock/tests/test_builder.py
+++ ftw/iframeblock/tests/test_builder.py
@@ -1,7 +1,7 @@
 from ftw.builder import Builder
 from ftw.builder import create
-from ftw.testbrowser import browsing
 from ftw.iframeblock.tests import FunctionalTestCase
+from ftw.testbrowser import browsing
 from ftw.testing import IS_PLONE_5
 
 
@@ -21,14 +21,4 @@ class TestIFrameBlockBuilder(FunctionalTestCase):
 
         browser.login().visit(content_page)
 
-        if IS_PLONE_5:
-            nodes = content_page.getChildNodes()
-            iframe_block_title = nodes._data[0].title
-
-            self.assertEqual(block_title, iframe_block_title,
-                             'We expect that a iframe block was created '
-                             'within the content page. Therefore the block '
-                             'must be the only child element of the content '
-                             'page having the title given.')
-        else:
-            self.assertTrue(len(browser.css('.sl-block')), 'Expect one block')
+        self.assertTrue(len(browser.css('.sl-block')), 'Expect one block')
