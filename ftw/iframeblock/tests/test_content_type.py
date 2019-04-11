d9aa4782e8c5fec31dba0ccd486974aca728da81
Matthias Osswald <m@osswald.li>
2019-04-11 09:42:13 +0200

    Use browser css from testbrowser
    Also I introduced a list comparison instead of multiple assertions.
    This makes it easier to find the errors if multiple would occur.

--- ftw/iframeblock/tests/test_content_type.py
+++ ftw/iframeblock/tests/test_content_type.py
@@ -1,11 +1,21 @@
 from ftw.builder import Builder
 from ftw.builder import create
+from ftw.iframeblock.tests import FunctionalTestCase
 from ftw.testbrowser import browsing
 from ftw.testbrowser.pages import factoriesmenu
-from ftw.iframeblock.tests import FunctionalTestCase
 
 
 class TestIFrameBlockContentType(FunctionalTestCase):
+
+    params_list = [
+        ('width', '100%'),
+        ('class', 'iframeblock loading'),
+        ('onload', 'onIframeLoaded(this)'),
+        ('src', 'http://www.google.com'),
+        ('height', '400'),
+        ('scrolling', 'auto'),
+        ('data-height-calculation-method', 'bodyOffset'),
+    ]
 
     def setUp(self):
         super(TestIFrameBlockContentType, self).setUp()
@@ -14,27 +24,19 @@ class TestIFrameBlockContentType(FunctionalTestCase):
     @browsing
     def test_block_can_be_added_with_factories_menu(self, browser):
         content_page = create(Builder('sl content page').titled(u'A page'))
-
         browser.login().visit(content_page)
         factoriesmenu.add('iFrame block')
         browser.fill({
             'URL': u'http://www.google.com',
             'Height': u'400',
         })
-
         browser.find_button_by_label('Save').click()
         browser.visit(content_page)
 
-        self.assertTrue(len(browser.css('.sl-block')), 'Expect one block')
+        expected = sorted([params[1] for params in self.params_list])
+        results = sorted([
+            browser.css('iframe').first.attrib[params[0]]
+            for params in self.params_list
+        ])
 
-        from lxml import html
-        tree = html.fromstring(browser.contents)
-        iframe = tree.xpath('//iframe')[0]
-
-        self.assertEqual(iframe.get('width'), '100%')
-        self.assertEqual(iframe.get('class'), 'iframeblock loading')
-        self.assertEqual(iframe.get('onload'), 'onIframeLoaded(this)')
-        self.assertEqual(iframe.get('src'), 'http://www.google.com')
-        self.assertEqual(iframe.get('height'), '400')
-        self.assertEqual(iframe.get('scrolling'), 'auto')
-        self.assertEqual(iframe.get('data-height-calculation-method'), 'bodyOffset')
+        self.assertListEqual(expected, results)
