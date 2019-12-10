4684b41cc0d5a34373de619bbece5261436c0b46
Tina Gerber <tina.gerber@4teamwork.ch>
2019-12-10 11:00:06 +0100

    Provide precompiled bundle

---
+++ build.js
@@ -0,0 +1,8 @@
+({
+    baseUrl: "./ftw/iframeblock/browser/resources",
+    paths: {
+        jquery: "empty:",
+    },
+    name: "main",
+    out: "./ftw/iframeblock/browser/resources/iframeblock-compiled.js"
+})
